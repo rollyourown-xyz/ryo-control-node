0a43616941fdc2a2defc2698a22193fe7f7bf4ba
xyzroller <xyzroller@rollyourown.xyz>
2021-07-05 07:41:29 +0200

    Added script directory variable to local-setup.sh

--- local-setup.sh
+++ local-setup.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-ansible-playbook -i configuration/inventory control-node/main.yml
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ansible-playbook -i "$SCRIPT_DIR"/configuration/inventory "$SCRIPT_DIR"/control-node/main.yml
